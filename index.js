e6baeb9289c2d31cc2941b123d2a37303c0fbe40
dsteed112e <dsteed112@gmail.com>
2020-06-24 10:29:06 -0600

    finished app

---
+++ index.js
@@ -0,0 +1,91 @@
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
+
+
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
+
+
+function addTodo(event){
+    event.preventDefault();
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+
+
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-button");
+    todoDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-button");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
+
+    todoInput.value = "";
+}
+
+function deleteCheck(event ){
+    const item = event.target;
+    if (item.classList[0] === 'trash-button'){
+        const todo = item.parentElement; 
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+    }
+
+    if(item.classList[0] === "complete-button"){
+        const todo = item.parentElement; 
+        todo.classList.toggle("completed");
+    }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(event.target.value){
+            
+            case "all":
+                todo.style.display = "flex";
+                break;
+                
+            case "completed":
+                if (todo.classList.contains("completed")){
+                todo.style.display = "flex";
+                } else {
+                todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")){
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+const quotes = [
+"“Wax on, wax off. Wax on, wax off.”",
+"“You trust the quality of what you know, not quantity.”",
+"“Either you karate do “yes” or karate do “no.” You karate do “guess so,” (get squished) just like grape.”",
+"“Never put passion in front of principle, even if you win, you’ll lose.”",
+"“Man who catches fly with chopstick can accomplish anything.”",
+"”Never trust spiritual leader who cannot dance.”",
+]
+
+const randomQuote = document.getElementById("quoteid").innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
